e42e85e18b45c88637fa00c45942948f24cb3317
Reinald Reynoso <reireynoso@gmail.com>
2019-03-14 18:28:18 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -17,9 +17,6 @@ def word_substituter(tweet)
   dic = dictionary
   tweet = tweet.split(" ")
   #keys = dic.keys
-  dic.each do |long,short|
-  
-  end
   i = 0
   tweet.collect do |word|
     dic.each |long, short|
